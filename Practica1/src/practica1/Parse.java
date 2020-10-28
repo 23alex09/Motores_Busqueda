c5da7f5734c3de9fa9f67f0ed66a4566d27ce250
Alejandro Hidalgo Acuña <73582288+23alex09@users.noreply.github.com>
2020-10-28 11:30:24 +0100

    Add files via upload
    Version 1. Leer un unico fichero, parsearlo e indexarlo en solar

---
+++ Practica1/src/practica1/Parse.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+
+/**
+ *
+ * @author alex_
+ */
+public class Parse {
+    
+    private Scanner sc;
+    
+    public void parseFile(String fileurl) throws FileNotFoundException, SolrServerException, IOException
+    {
+        sc = new Scanner(new File(fileurl));
+        HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/Practica1").build();
+        
+        while(sc.hasNextLine())
+        {
+            SolrInputDocument doc = new SolrInputDocument();
+            String line = sc.nextLine();
+            String id = line;
+            doc.addField("id", id);
+            line = sc.nextLine();
+            String titulo = "";
+            while(!line.isEmpty())
+            {
+                titulo = titulo + " " + line;
+                line = sc.nextLine();   
+            }
+            doc.addField("title", titulo);           
+            line = sc.nextLine();            
+            String texto = "";
+            while(!line.contains("**********"))
+            {
+                texto = texto + " " + line;
+                line = sc.nextLine();                
+            }            
+            doc.addField("text", texto);           
+            solr.add(doc);            
+        }
+        solr.commit();
+        sc.close();
+    }   
+}
---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+import java.io.IOException;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+
+/**
+ *
+ * @author alex_
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SolrServerException, IOException {
+        
+        Parse p = new Parse();
+        
+        p.parseFile("ficheros_lisa/LISA0.001");
+        
+    }
+    
+}
