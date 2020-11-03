df0829cf370efafef17f3d005f1feddff225e77a
alex_ <alex_@DESKTOP-1V2ABI1>
2020-11-03 16:46:14 +0100

    Solucionada lectura del ID sin necesidad de un contador. Nueva funcionalidad: Leer el fichero de queries (falta probar)

---
+++ Practica1/src/practica1/ParseQueries.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+
+/**
+ *
+ * @author alex_
+ */
+public class ParseQueries {
+    
+    private Scanner sc;
+    private SolrDocumentList [] documentos;
+    public void parseFile(String fileurl) throws FileNotFoundException, SolrServerException, IOException
+    {
+        sc = new Scanner(new File(fileurl));
+        HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/Practica1").build();
+        int j = 0;
+        while(sc.hasNextLine())
+        {
+            String line = sc.nextLine();
+            int idQ = Integer.parseInt(line);
+            System.out.println(idQ);
+            line = sc.nextLine();
+            String texto = line;
+            while(!line.contains("#"))
+            {
+                line = sc.nextLine();
+                texto = texto + " " +line;
+            }
+            
+            SolrQuery query = new SolrQuery();
+            String [] palabras = texto.split(" ");
+            String consulta = "";
+            for(int i = 0; i<10; i++)
+            {
+                consulta = consulta + palabras[i] + " ";
+            }
+            
+            query.setQuery(consulta);
+            QueryResponse rsp = solr.query(query);
+            SolrDocumentList docs = rsp.getResults();
+            //for (int i = 0; i < docs.size(); ++i) {
+              //  System.out.println(docs.get(i));
+            //}
+            documentos[j] = docs;
+            j++;
+        }
+    }
+
+    public SolrDocumentList[] getDocumentos() {
+        return documentos;
+    }
+    
+    
+}
